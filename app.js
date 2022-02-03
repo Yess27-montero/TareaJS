dc500b8c3633d3d001684d1bbdb371693c2e6b0d
Yessica <jmonteroarriola@gmail.com>
2022-02-03 15:05:28 -0600

    Agregar un archivo html y js para la declaracion de variable

---
+++ app.js
@@ -0,0 +1,24 @@
+
+//String: es una cadena de txto. 
+var nombre = 'Yessica';
+console.log(nombre);
+
+//number: es un real o un entero.
+var x = '10';
+console.log(x * 3); 
+
+//boolean: true/false.
+var esdeportista = true; 
+console.log(esdeportista); 
+
+//Array: es un arrglo.
+let autos = ["Toyota", "Nissan"];
+console.log(autos [1]);
+
+//Objeto
+var persona = {nombre: 'Isabella', animal: 'conejo'};
+console.log(persona.animal);
+
+//Undefined
+let numero;
+console.log(numero);
